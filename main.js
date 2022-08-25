0bbbd99b837b72b169ee014cff1bd3d984473c4a
Naman-3010 <91064945+Naman-3010@users.noreply.github.com>
2022-08-25 10:51:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,20 +5,18 @@ rightWristX=0;
 rightWristY=0;
 scoreLeftWrist=0;
 scoreRightWrist=0;
-songName=0;
 
-
+/*songValue="";*/
 
 function preload(){
-
-
-
-    song=loadSound("");
+    /*songValue=document.getElementById("choose_file").value;*/
+    
 
 }
 
 function setup() { 
-    canvas = createCanvas(600, 500); 
+    canvas = createCanvas(600, 500);
+
     canvas.center(); 
     video = createCapture(VIDEO); 
     video.hide();
@@ -59,11 +57,52 @@ function draw() {
     image(video, 0, 0, 600, 500);
     fill("#0000FF");
     stroke("#FF0000");
+
+    if(scoreRightWrist>0.2){
+
+    
+    circle(rightWristX,rightWristY,20);
+
+    if(rightWristY>0 &&rightWristY<=100){
+        document.getElementById("speed").innerHTML="Speed=0.5x";
+        song.rate(0.5);
+
+    }
+    else if(rightWristY>100&&rightWristY<=200){
+        document.getElementById("speed").innerHTML="Speed=1x";
+        song.rate(1);
+    }
+    else if(rightWristY>200&&rightWristY<=300){
+        document.getElementById("speed").innerHTML="Speed=1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY>300&&rightWristY<=400){
+        document.getElementById("speed").innerHTML="Speed=2x";
+        song.rate(2);
+    }
+    else if(rightWristY>400&&rightWristY<=500){
+        document.getElementById("speed").innerHTML="Speed=2.5x";
+        song.rate(2.5);
+    }
+}
+    
+    if(scoreLeftWrist > 0.2) { 
+        circle(leftWristX,leftWristY,20); 
+        InNumberleftWristY = Number(leftWristY); 
+        remove_decimals = floor(InNumberleftWristY); 
+        volume = remove_decimals/500; 
+        document.getElementById("volume").innerHTML = "Volume = " + volume; 
+        song.setVolume(volume); 
+    }
+
+
+    
+   
+
+
 }
 
-   
-    
-    function play(){
+function play(){
     song.play();
     song.setVolume(1);
     song.rate(1);
